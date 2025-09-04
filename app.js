f6911ceec3a649eaeb5f8348ec0377a19200a94c
Alvaro Milantonio <alvarolacortee@gmail.com>
2025-09-04 11:46:58 -0300

    Adicionando Title e body ao botão

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 let button = document.getElementById('btnUsers');
+let div = document.getElementById('out');
 
 button.addEventListener('click', function () {
     // Assíncrona Async Chaining
-    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    fetch('https://jsonplaceholder.typicode.com/posts/')
         .then(function (response) {
             return response.json();
         })
-        .then(function (json) {
-            console.log(json);
+        .then(function (data) {
+            // console.log(json);
+            div.textContent = `Title: ${data[0].title}, Body: ${data[0].body}`;
+            // let listaOrdenada = '<ol>';
+            for (usuario in data){
+                // let item = '<li>'
+                // '<li>' + `Title: ${usuario.title}` + '</li>'
+
+            }
         });
 });
