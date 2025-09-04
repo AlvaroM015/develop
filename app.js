79f633421b376ab64a6aa11b78bd0ef8a7d52533
Alvaro Milantonio <alvarolacortee@gmail.com>
2025-09-04 11:17:39 -0300

    Revalidando o JS

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-let button = document.getElementById('btnUsers')
+let button = document.getElementById('btnUsers');
 
-button.addEventListener ('click', function (){
-
-    console.log('clicou!');
-    
-})
+button.addEventListener('click', function () {
+    // Assíncrona Async Chaining
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            console.log(json);
+        });
+});
